b5c95a589640cfe1f631a9b26138e4c35c8d1a5b
Eli-b-good <eli.i.henderson95@gmail.com>
2020-09-22 15:46:17 -0400

    initial commit and auth form testing

---
+++ src/components/Login.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  return <div>Hello from the login page</div>;
+};
---
+++ src/components/Signup.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  return <div>Hello from the signup page</div>;
+};
