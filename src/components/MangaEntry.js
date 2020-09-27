5c2ceae64082ab14bd7b9f80b89d8fdc260d990d
Eli-b-good <eli.i.henderson95@gmail.com>
2020-09-27 14:41:03 -0400

    testing fetching info from kitsu api

--- src/components/MangaEntry.js
+++ src/components/MangaEntry.js
@@ -1,30 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
+import { useLocation } from "react-router-dom";
+import MangaList from "./MangaList";
 
 const MangaEntry = () => {
   const [manga, setManga] = useState("");
+  const [mangaObj, setMangaObj] = useState({
+    title: "",
+    chapterAmount: 0,
+    synopsis: "",
+    malScore: 0,
+    mangaImage: "",
+    malUrl: "",
+  });
   const [mangaList, setMangaList] = useState([]);
   let userInfo = localStorage.getItem("user");
   const token = JSON.parse(userInfo).token;
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    await axios
-      .post("https://eli-manga-api.herokuapp.com/api/manga", {
-        title: manga,
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-
+  useEffect(() => {
     axios
       .get("https://eli-manga-api.herokuapp.com/api/manga")
       .then((res) => {
@@ -34,6 +31,38 @@ const MangaEntry = () => {
       .catch((e) => {
         console.log(e);
       });
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await await axios
+      .get(`https://kitsu.io/api/edge/manga?filter[text]=${manga}`)
+      .then((res) => {
+        const mangainfo = res.data.data[0];
+        console.log(res.data.data[0]);
+        setMangaObj({
+          title: "",
+          chapterAmount: 0,
+          synopsis: "",
+          malScore: 0,
+          mangaImage: "",
+          malUrl: "",
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    // await axios
+    //   .post("https://eli-manga-api.herokuapp.com/api/manga", {
+    //     title: manga,
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //     window.location.reload(false);
+    //   })
+    //   .catch((e) => {
+    //     console.log(e);
+    //   });
   };
 
   return (
