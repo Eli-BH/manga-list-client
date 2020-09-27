66183a0f648749240738dfeb1e30b206dba955c3
Eli-b-good <eli.i.henderson95@gmail.com>
2020-09-26 23:55:04 -0400

    testing GET list, need to try different state settings

--- src/components/MangaList.js
+++ src/components/MangaList.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 
 const MangaList = () => {
+  const [manga, setManga] = useState(null);
   let userInfo = localStorage.getItem("user");
   const token = JSON.parse(userInfo).token;
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
   const handleButton = async () => {
     await axios
       .get("https://eli-manga-api.herokuapp.com/api/manga")
       .then((res) => {
         console.log(res.data);
+        setManga(res.data);
       })
       .catch((e) => {
         console.log(e);
-        console.log(`Bearer ${token}`);
       });
   };
   return (
     <div>
-      {token} <Button onClick={() => handleButton()}>Button</Button>
+      {JSON.stringify(manga, null, 2)}{" "}
+      <Button onClick={() => handleButton()}>Button</Button>
     </div>
   );
 };
